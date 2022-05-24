e39345a7b87a30138063b9ecbeded635db3d4f88
CCom20 <cole.d.comstock@gmail.com>
2022-05-23 19:49:06 -0600

    HTML updated, working on backend app.

--- app.py
+++ app.py
@@ -1,184 +1,139 @@
-import pandas as pd
-import numpy as np
-import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+from flask import Flask, redirect, url_for, request, render_template, session
+import pandas as pd 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        guess = request.form["guessField"]
+        wrong_letters = request.form["wrongLettersField"]
+        right_letters = request.form["rightLettersField"]
+        return redirect("/", code=302)
+        
+    return render_template("index.html")
+
+# def form():
+#     if request.method == "POST":
+#         guess = request.form["guessField"]
+#         wrong_letters = request.form["wrongLettersField"]
+#         right_letters = request.form["rightLettersField"]
+
+#         pet = Pet(name=name, lat=lat, lon=lon)
+#         db.session.add(pet)
+#         db.session.commit()
+#         return redirect("/", code=302)
+#     return render_template("index.html")
+
+while guess_counter < 7:
     
-    new_list_df = pd.read_csv("data/english3.txt", sep=" ", header=None)
-    new_list_df.rename(columns={0: 'W'}, inplace=True)
+    right_letters = [x for x in guess if x not in wrong_letters]
 
-    # AVAILABLE LETTERS
+    check_letters = [x for x in right_letters if x not in right_spots]
 
-    available_word_list = []
+    exclude_list = []
 
-
-    for i, r in new_list_df[new_list_df['W'].str.len() == 5].iterrows():
-        
-        word = r['W']
-        word_letters = list(r['W'])
-            
-        available_word_list.append({'Word': word, 'Letters': word_letters})
-
-    guess_counter = 0
-
-    def word_check(guess):
-        w = ''
-        for x in guess:
-            w += x
-        return w
-
-    while guess_counter < 7:
-        
-        guess = list(input('What was your guess? '))
-
-        wrong_letters = list(input('Which letters are grey? '))
-
-        right_letters = [x for x in guess if x not in wrong_letters]
-
-        right_spots = list(input('Which letters are green? Leave blank if none, press Enter. '))
-
-        check_letters = [x for x in right_letters if x not in right_spots]
-
-        exclude_list = []
-
-        # Exclude all words where the letter is correct but the index of the letter is not
-        for x in check_letters:
-
-            for w in available_word_list:
-
-                try:
-                    if x not in w['Letters']:
-                        exclude_list.append(w)
-                    else:
-                        next
-                except:
-                    next
-
-                try:
-                    if guess.index(x) == w['Letters'].index(x):
-                        exclude_list.append(w)
-                    else:
-                        next
-                except:
-                    next
-
-        # Exclude all words containing the wrong letters
-        for x in wrong_letters:
-            for w in available_word_list:
-                if x in w['Letters']:
-                    exclude_list.append(w)
-                else:
-                    next
-
-        # Exclude all words where letter is not in the right index
-
-        for x in right_spots:
-            for w in available_word_list:
-                try:
-                    if guess.index(x) != w['Letters'].index(x):
-                        exclude_list.append(w)
-                    else:
-                        next
-                except:
-                    exclude_list.append(w)
-
-        # Reset available words list
-
-        available_word_list = [x for x in available_word_list if x not in exclude_list]
-
-        # Get each letter's weight/value
-
-        letter_count_dictionary = {}
-        for x in available_word_list:
-            for l in x['Letters']:
-                if l not in letter_count_dictionary:
-                    letter_count_dictionary[f'{l}'] = 0
-                    for a in available_word_list:
-                        if l in a['Letters']:
-                            letter_count_dictionary[f'{l}'] +=1
-                else:
-                    next
-
-        top_words = {}
+    # Exclude all words where the letter is correct but the index of the letter is not
+    for x in check_letters:
 
         for w in available_word_list:
-            word_weight = 0
 
-            for l in w['Word']:
+            try:
+                if x not in w['Letters']:
+                    exclude_list.append(w)
+                else:
+                    next
+            except:
+                next
 
-                word_weight += letter_count_dictionary[l]
+            try:
+                if guess.index(x) == w['Letters'].index(x):
+                    exclude_list.append(w)
+                else:
+                    next
+            except:
+                next
 
-            if w['Word'] not in top_words:
-                word = w['Word']
-                top_words[word] = word_weight
+    # Exclude all words containing the wrong letters
+    for x in wrong_letters:
+        for w in available_word_list:
+            if x in w['Letters']:
+                exclude_list.append(w)
             else:
                 next
 
-        # {k: v for k, v in sorted(top_words.items(), key=lambda item: item[1], reverse=True)}
-        top_list_exlude = {}
-        for x in top_words:
-            word_sum = 0
-            for l in x:
-                word_sum += x.count(l)
-            if word_sum > 5:
-                top_list_exlude[f'{x}'] = top_words[x]
+    # Exclude all words where letter is not in the right index
 
-        # Reset top words list based on no repeats and order by their expected information value
-        suggested_words = [{x: top_words[x] for x in top_words if x not in top_list_exlude}]
-    #     top_words = {k: v for k, v in sorted(suggested_words[0].items(), key=lambda item: item[1], reverse=True)} 
-        for x in top_words:
-            print(f'Word: {x}, Information Value: {round(top_words[x] / len(top_words), 2)}')
+    for x in right_spots:
+        for w in available_word_list:
+            try:
+                if guess.index(x) != w['Letters'].index(x):
+                    exclude_list.append(w)
+                else:
+                    next
+            except:
+                exclude_list.append(w)
 
-        guess_counter += 1
-        
-        if guess_counter == 7:
-            break
-        elif (len(right_spots) == 5) and (guess == right_spots):
-            correct_word = word_check(guess)
-            print(f'Congrats! The word is {correct_word}.')
-            break
+    # Reset available words list
+
+    available_word_list = [x for x in available_word_list if x not in exclude_list]
+
+    # Get each letter's weight/value
+
+    letter_count_dictionary = {}
+    for x in available_word_list:
+        for l in x['Letters']:
+            if l not in letter_count_dictionary:
+                letter_count_dictionary[f'{l}'] = 0
+                for a in available_word_list:
+                    if l in a['Letters']:
+                        letter_count_dictionary[f'{l}'] +=1
+            else:
+                next
+
+    top_words = {}
+
+    for w in available_word_list:
+        word_weight = 0
+
+        for l in w['Word']:
+
+            word_weight += letter_count_dictionary[l]
+
+        if w['Word'] not in top_words:
+            word = w['Word']
+            top_words[word] = word_weight
         else:
             next
 
-    return render_template("index.html")
-# create route that renders index.html template
+    # {k: v for k, v in sorted(top_words.items(), key=lambda item: item[1], reverse=True)}
+    top_list_exlude = {}
+    for x in top_words:
+        word_sum = 0
+        for l in x:
+            word_sum += x.count(l)
+        if word_sum > 5:
+            top_list_exlude[f'{x}'] = top_words[x]
+
+    # Reset top words list based on no repeats and order by their expected information value
+    suggested_words = [{x: top_words[x] for x in top_words if x not in top_list_exlude}]
+    top_words = {k: v for k, v in sorted(suggested_words[0].items(), key=lambda item: item[1], reverse=True)} 
+    for x in top_words:
+        print(f'Word: {x}, Information Value: {round(top_words[x] / len(top_words), 2)}')
+
+    guess_counter += 1
+    
+    if guess_counter == 7:
+        break
+    elif (len(right_spots) == 5) and (guess == right_spots):
+        correct_word = word_check(guess)
+        print(f'Congrats! The word is {correct_word}.')
+        break
+    else:
+        next
 
 
 
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
-
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(pet_data)
 
 
 if __name__ == "__main__":
---
+++ js/wordle-helper.js
@@ -0,0 +1,5 @@
+console.log("Hello!")
+
+var csv = getCsv("../data/english.csv"); 
+
+var data = $.csv.toObjects(csv);
