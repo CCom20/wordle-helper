7536f65aace489757d7237677e6d40692531cbc8
CCom20 <cole.d.comstock@gmail.com>
2022-05-29 10:07:05 -0600

    Updated with config file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, redirect, url_for, request, render_template, session
 from helper import reload_available_words
+from config import secret_key
 import pandas as pd 
 import random
 
 app = Flask(__name__)
-app.secret_key = random.seed(a=16)
+app.secret_key = secret_key
 
 
 random_phrase = random.choice(['From Wordle Dud to Wordle Stud', 'From Wordle Zero to Wordle Hero', 
@@ -43,29 +44,13 @@ def home():
             else:
                 session['Words'] = reload_available_words(guess, wrong_letters, right_spots, session['Words'])
 
-            # if session['guess_counter'] == 6 and len(right_spots) !=5:
-            #     game_over = 'Sorry, it looks like you were not able to guess the word.'
-            # elif (len(right_spots) == 5) and (guess == right_spots):
-            #     correct_word = word_check(guess)
-            #     game_over = f'Congrats! The word is {correct_word}.'
-            # else:
-            #     next
             
             return render_template("index.html", result=session['Words'], random_phrase=random_phrase)
 
-        else:
-            return render_template("index.html", result='', random_phrase=random_phrase)
-
-            
-
-    # OTHERWISE, SET ALL SESSION DATA
     else:
-        
-        # GUESS COUNTER
         session['guess_counter'] = 0
         session['loaded'] = 0
-
-        return redirect("/")
+        return render_template("index.html", result='', random_phrase=random_phrase)
 
 if __name__ == "__main__":
     session.init_app(app)
